0900b4b9744c40b6576ec3de1fa14db531913c7d
Jan Brauer <jan@jimdo.com>
2011-05-13 10:17:14 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Cuke stats from Jenkins
+=======================
+
+Aggregate stats from Jenkins to classify Cucumber features.
+For a given failure rate a feature could be classified as flaky or flickering.
---
+++ cucumber_stats.py
@@ -0,0 +1,60 @@
+import itertools
+import json
+import urllib
+
+
+def gather_jobs(job_name):
+    url = "http://hudson/job/%s/api/json" % job_name
+    return json.load(urllib.urlopen(url))
+
+
+def fetch_report_for_build(job_name, build):
+    url = "http://hudson/job/%s/%s/testReport/api/json" % (job_name, build)
+    return json.load(urllib.urlopen(url))
+
+
+def cases_for_build_report(report):
+    raw_cases = [c['cases'] for c in report['suites']]
+    return list(itertools.chain.from_iterable(raw_cases))
+
+
+def group_tests_by_status(cases):
+    return [(t['name'], t['status']) for t in cases]
+
+
+def extract_group_for_build(b):
+    report = fetch_report_for_build(b)
+    cases = cases_for_build_report(report)
+    return group_tests_by_status(cases)
+
+
+def mapper(tests):
+    '''
+    Expects to be called with a list of grouped tests
+
+    >>> tests = [('foo', 'PASSED'), ('foo', 'FAILED'), ('bar', 'PASSED')]
+    >>> mapper(tests)
+    {'foo': ['FAILED', 'PASSED'], 'bar': ['PASSED']}
+    '''
+    mapped_tests = {}
+    tests.sort()
+    for key, group in itertools.groupby(tests, lambda x: x[0]):
+        mapped_tests[key] = [test[1] for test in group]
+    return mapped_tests
+
+
+def dump_results(results):
+    for r in results:
+        print "%s\t%s" % r
+
+if __name__ == '__main__':
+    all_dev_cucumber_jobs = gather_jobs('dev-cucumber')
+    builds = [b['number'] for b in all_dev_cucumber_jobs['builds']]
+    for b in builds:
+        try:
+            report = fetch_report_for_build('dev-cucumber', b)
+            cases = cases_for_build_report(report)
+            results = group_tests_by_status(cases)
+            dump_results(results)
+        except ValueError:
+            pass
